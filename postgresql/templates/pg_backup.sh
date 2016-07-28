3354e990d5c1da61ca3806c44814e3c2dbc939c4
Gregory Duchatelet <greg@easyflirt.com>
2016-07-28 16:22:30 +0200

    pg backup fail detection

--- postgresql/templates/pg_backup.sh
+++ postgresql/templates/pg_backup.sh
@@ -17,7 +17,12 @@ then
 fi
 
 mkdir -p "$PGBACKUPDIR/new/datadir"
-/usr/lib/postgresql/{{ pg_version }}/bin/pg_basebackup -l "daily backup" -D "$PGBACKUPDIR/new/datadir" -R -P -F p
+
+pg_basebackup_failed=0
+if ! /usr/lib/postgresql/{{ pg_version }}/bin/pg_basebackup -l "daily backup" -D "$PGBACKUPDIR/new/datadir" -R -P -F p
+then
+    pg_basebackup_failed=1
+fi
 
 
 mkdir -p "$PGBACKUPDIR/new/dump"
@@ -25,6 +30,13 @@ mkdir -p "$PGBACKUPDIR/new/dump"
 for db in $( psql -tAqnc "SELECT datname FROM pg_database WHERE datistemplate = false;" )
 do
 	mkdir -p "$PGBACKUPDIR/new/dump/$db"
+
+    # if pg_basebackup failed, dump whole db in pg_restore dump format
+    if [ $pg_basebackup_failed -gt 0 ]
+    then
+            pg_dump -Fc --clean --if-exists "$db" | lzma > "$PGBACKUPDIR/new/dump/$db/_WHOLEDB_.dump.lzma"
+    fi
+
 	for table in $( psql -tAqnc "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog') ORDER BY table_schema,table_name;" $db )
 	do
 		pg_dump --table "$table" --clean --if-exists "$db" | lzma $COMPRESSLEVEL >"$PGBACKUPDIR/new/dump/$db/$table.sql.lzma"
