03f1dcb2afc9c3c019c76e7698121a56e07cd089
Gregory Duchatelet <greg@easyflirt.com>
2020-12-29 08:20:54 +0100

    pg_backup without psqlrc

--- postgresql/templates/pg_backup.sh
+++ postgresql/templates/pg_backup.sh
@@ -27,7 +27,7 @@ fi
 
 mkdir -p "$PGBACKUPDIR/new/dump"
 # pg_dump ...
-for db in $( psql -tAqnc "SELECT datname FROM pg_database WHERE datistemplate = false;" )
+for db in $( psql -XtAqnc "SELECT datname FROM pg_database WHERE datistemplate = false;" )
 do
 	mkdir -p "$PGBACKUPDIR/new/dump/$db"
 
@@ -37,7 +37,7 @@ do
             pg_dump -Fc --clean --if-exists "$db" | lzma > "$PGBACKUPDIR/new/dump/$db/_WHOLEDB_.dump.lzma"
     fi
 
-	for table in $( psql -tAqnc "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog') ORDER BY table_schema,table_name;" $db )
+	for table in $( psql -XtAqnc "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog') ORDER BY table_schema,table_name;" $db )
 	do
 		pg_dump --table "$table" --clean --if-exists "$db" | lzma $COMPRESSLEVEL >"$PGBACKUPDIR/new/dump/$db/$table.sql.lzma"
 		vacuumdb -e -v -z --table "$table" "$db" &
