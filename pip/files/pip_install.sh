d220fe247a5647575c073a2445768327e88e7962
Gregory Duchatelet <greg@easyflirt.com>
2021-07-29 17:38:57 +0200

    fix pip with python 3.5

--- pip/files/pip_install.sh
+++ pip/files/pip_install.sh
@@ -2,4 +2,4 @@
 
 
 #curl https://bootstrap.pypa.io/get-pip.py -o - | python
-curl https://bootstrap.pypa.io/get-pip.py -o - | python3
+curl https://bootstrap.pypa.io/get-pip.py -o - | python3 || curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o - | python3
