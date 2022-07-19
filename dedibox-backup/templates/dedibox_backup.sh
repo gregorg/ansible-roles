3630388af8145f2d5dff33257a9698ca6f100789
Gregory Duchatelet <greg@easyflirt.com>
2022-07-19 09:18:41 +0200

    add nocache to dedibox backup

--- dedibox-backup/templates/dedibox_backup.sh
+++ dedibox-backup/templates/dedibox_backup.sh
@@ -3,10 +3,10 @@
 log="/var/log/dedibox_backup.log"
 
 echo "$( date -R ) :: STARTING BACKUP" >>$log
-if /usr/sbin/backup-manager --no-upload --verbose >>$log
+if nocache /usr/sbin/backup-manager --no-upload --verbose >>$log
 then
 	echo "$( date -R ) :: STARTING UPLOAD" >>$log
-	/usr/sbin/backup-manager --upload --verbose >>$log
+	nocache /usr/sbin/backup-manager --upload --verbose >>$log
 fi
 echo "$( date -R ) :: BACKUP DONE" >>$log
 
